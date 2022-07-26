c2a0a622aef439ea90dc7e584b95deca5f10a10b
Daniel Sutanto Sutanto <108246001+MoonCaik@users.noreply.github.com>
2022-07-26 23:01:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import random
+import sys
+from pygame.locals import*
+from ship import *
+#loading pygame
+pygame.init()
+#displays info onto screen
+screen_info=pygame.display.Info()
+
+#setting up the size of the window
+size = (width,height)=(int(screen_info.current_w),
+int(screen_info.current_h))
+
+screen= pygame.display.set_mode(size)
+
+#Clock 
+clock = pygame.time.Clock()
+
+level_total = 4
+current_level= 1
+asteroid_count = 3
+Player = Ship((20,200))
+def main():
+  while current_level<=level_total:
+    clock.tick(60)
+    for event in pygame.event.get():
+      if event.type == QUIT: 
+        sys.exit()
+      if event.type==pygame.KEYDOWN: #if any key is pressed
+        if event.key==pygame.K_RIGHT:
+          Player.speed[0]=10
+        if event.key==pygame.K_LEFT:
+          Player.speed[0]=-10
+        if event.key==pygame.K_UP:
+          Player.speed[1]=-10
+        if event.key==pygame.K_DOWN:
+          Player.speed[1]=10
+    screen.fill((0,0,100))  
+    screen.blit(Player.image,Player.rect)
+    pygame.display.flip()
+
+if __name__=="__main__":
+  main()
